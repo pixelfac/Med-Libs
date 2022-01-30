e8506f97762b83427a2cfaf66f4c6609a2594651
Alexia Rangel K <73617301+alexiarangelk@users.noreply.github.com>
2022-01-30 01:41:01 -0500

    QnA Complete

--- Assets/Scripts/ReadCSV_qna.cs
+++ Assets/Scripts/ReadCSV_qna.cs
@@ -13,7 +13,7 @@ public class ReadCSV_qna : MonoBehaviour
 
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         //calls function below. 
         ReadCSVqna();
